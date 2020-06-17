c68e288e0833850c5d2b54ec62042ebe6d275115
Nikita Krokosh <de.lsnk@gmail.com>
2020-06-17 23:04:35 +1000

    Добавил еврооблигации

--- src/parsers/moex/bonds/__init__.py
+++ src/parsers/moex/bonds/__init__.py
@@ -4,6 +4,7 @@ from .constants import PARSING_URLS
 from .parser import parse
 
 
+# TODO: выделить общий базовый класс для парсеров
 parsers = [
     functools.partial(parse, url, description)
     for url, description in PARSING_URLS.items()
