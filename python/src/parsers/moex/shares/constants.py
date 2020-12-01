b162c24d889521d89b96ff8cf3a3f4a4bda23525
Nikita Krokosh <de.lsnk@gmail.com>
2020-12-01 18:38:24 +1000

    Добавил USD/EUR etf

--- python/src/parsers/moex/shares/constants.py
+++ python/src/parsers/moex/shares/constants.py
@@ -3,9 +3,13 @@ from ..constants import MOEX_API_BASE_URL
 
 SHARES_AND_DR_URL = f'{MOEX_API_BASE_URL}/shares/boards/TQBR/securities.xml'
 ETFS_URL = f'{MOEX_API_BASE_URL}/shares/boards/TQTF/securities.xml'
+ETFS_USD_URL = f'{MOEX_API_BASE_URL}/shares/boards/TQTD/securities.xml'
+ETFS_EUR_URL = f'{MOEX_API_BASE_URL}/shares/boards/TQTE/securities.xml'
 
 
 PARSING_URLS = {
     SHARES_AND_DR_URL: 'MOEX. Акции и ДР',
     ETFS_URL: 'MOEX. ETF',
+    ETFS_USD_URL: 'MOEX. ETF (USD)',
+    ETFS_EUR_URL: 'MOEX. ETF (EUR)',
 }
